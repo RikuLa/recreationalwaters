175d1f707dc6c078834cbb140c8960b4e0c72159
Riku <riku@neverthink.tv>
2018-01-28 16:23:03 +0200

    add three best options

--- src/Map.js
+++ src/Map.js
@@ -16,10 +16,11 @@ export default class MapContainer extends Component {
     super(props)
 
     this.state = {
-      lat: LATITUDE_OF_OTANIEMI,
-      lng: LONGITUDE_OF_OTANIEMI,
+      currentLat: LATITUDE_OF_OTANIEMI,
+      currentLng: LONGITUDE_OF_OTANIEMI,
       zoom: 12,
       features: null,
+      bestThreeBeaches: null,
       minTemperature: 10,
       algae: false,
       amountOfValidBeaches: 0
@@ -31,7 +32,7 @@ export default class MapContainer extends Component {
   }
 
  render () {
-   const position = [this.state.lat, this.state.lng]
+   const position = [this.state.currentLat, this.state.currentLng]
    const currentPosIcon = L.icon({
      iconUrl: 'http://www.clipartpanda.com/clipart_images/location-icon-195x300-38755946/download',
      iconSize: [30, 45]
@@ -39,15 +40,42 @@ export default class MapContainer extends Component {
    return  (
      <div id="mapContainer">
        {this.renderBeachCounter()}
+       {this.renderBestThreeBeaches()}
      <Map center={position} zoom={this.state.zoom}>
        <TileLayer url='http://{s}.tiles.wmflabs.org/bw-mapnik/{z}/{x}/{y}.png'/>
-       <Marker position={position} icon={currentPosIcon}/>
+       <Marker position={[LATITUDE_OF_OTANIEMI, LONGITUDE_OF_OTANIEMI]} icon={currentPosIcon}/>
        {this.renderMarkers()}
        {this.renderMuikkus()}
      </Map>
        {this.renderControls()}
      </div>
    )
+ }
+
+ renderBestThreeBeaches(){
+    if(this.state.bestThreeBeaches.length){
+      const bestThree = this.state.bestThreeBeaches.map(f => {
+        return <li>{f.properties.uimavesini} / C:{f.properties.temperature}
+        <button onClick={this.zoomTo(f.geometry.coordinates)}>Move To!</button>
+        </li>
+      })
+      return <div className={"bestThreeList"}>
+        <div className="threeHeader">Best three choices:</div>
+        <ul>
+          {bestThree}
+        </ul>
+      </div>
+    }
+ }
+
+ zoomTo = (coords) => {
+    return () => {
+      this.setState({
+        currentLat: coords[1],
+        currentLng: coords[0],
+        zoom: 15
+      })
+    }
  }
 
  renderMuikkus(){
@@ -124,8 +152,11 @@ export default class MapContainer extends Component {
       const { minTemperature, algae } = this.state
       return Math.floor(f.properties.temperature) >= minTemperature && f.properties.algae === algae
     })
+
+    const bestThreeBeaches = features.slice(0,3)
    this.setState({
-     features
+     features,
+     bestThreeBeaches
    })
  }
 }
